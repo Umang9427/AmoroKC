61b89a1d09846d5fdb4871aa27f3e6eb1d95d73c
Umang9427 <umang9427@gmail.com>
2024-05-14 19:33:41 +0530

    Mobile version and products pop up on hover

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-//Blur the hero Image on hover of navigation bar
-const loungeimage = document.getElementById("loungeimage");
+//------------------------ Blur the hero Image on hover of navigation bar -----------------------
+
+const loungeimage = document.getElementById("backgroundimage");
 const navcontainer = document.getElementById("navcontainer");
 
 navcontainer.addEventListener("mouseover", function() {
-    loungeimage.style.filter = "blur(10px)";
+    loungeimage.style.filter = "blur(5px)";
 });
 
 navcontainer.addEventListener("mouseout", function() {
@@ -11,236 +12,120 @@ navcontainer.addEventListener("mouseout", function() {
 });
 
 
-// Zoom the image on hovering near a product
+// ----------------------------------- Zoom the hero image and show product on hovering near a product --------------------------------
 
-const productlabel = document.getElementById("productlabel");
-const product1 = document.getElementById("product1");
-const product2 = document.getElementById("product2");
-const product3 = document.getElementById("product3");
-const product4 = document.getElementById("product4");
-const product5 = document.getElementById("product5");
-const product6 = document.getElementById("product6");
-const product7 = document.getElementById("product7");
-const product8 = document.getElementById("product8");
-const circles = document.querySelectorAll('#circlecontainer')
-const products = document.querySelectorAll('.product')
+
+const ripple1 = document.getElementById("ripple1");
+const ripple2 = document.getElementById("ripple2");
+const closelabel = document.getElementById("close");
+
+const product = document.getElementById("productimage");
+const ripples = document.querySelectorAll('.ripple-effect');
+
+
+closelabel.addEventListener("click", function(){
+  loungeimage.style.transform = "scale(1)";
+  loungeimage.style.transformOrigin = "47.3% 59.5%";
+  loungeimage.style.filter= "blur(0px)";
+  product.style.display = "none";
+  for (const ripple of ripples) {
+    ripple.style.display = "block";
+  }
+  closelabel.style.display = "none";
+  closelabel.style.transform = "translateY(0px)  translateX(0px)" ;
+  product.style.padding = "0px 0px"
+});
+
+
+product.addEventListener("mouseout", function(){
+  //On hovering outside a product image, the image should disappear
+  loungeimage.style.transform = "scale(1)";
+  loungeimage.style.transformOrigin = "47.3% 59.5%";
+  loungeimage.style.filter= "blur(0px)";
+  product.style.display = "none";
+  for (const ripple of ripples) {
+    ripple.style.display = "block";
+  }
+  closelabel.style.display = "none";
+  closelabel.style.transform = "translateY(0px)  translateX(0px)" ;
+  product.style.padding = "0px 0px"
+});
+
+
 
 /*------------------------ Product 1 ------------------------------- */
-product1.addEventListener("mouseover", function() {
-  for (const circle of circles) {
-    circle.style.display = "none";
-  }
-  loungeimage.style.transform = "scale(2)";
-  loungeimage.style.transformOrigin = "47.3% 59.5%";
-  for (const product of products) {
-    const i=0;
-    i=i+1;
-    if (i==1){continue;}
-    else{ product.style.display = "none"; }
+ripple1.addEventListener("mouseover", function() {
+  product.style.display = "block";
+  product.src = "./img/Product_Images/Von Vene Kansas.jpeg";
+  product.onload = function(){
+    const imageHeight = product.naturalHeight;
+    const imageWidth = product.naturalWidth;
+    const aspectratio = imageWidth/imageHeight;
+    product.style.aspectRatio = "" + aspectratio;
+  
+    const screenWidth = window.innerWidth; // Get screen width in pixels
+    const screenHeight = window.innerHeight;
+    const desiredPercentage = 70; // Desired percentage of screen width (Product Image width)
+    const actualimageWidth = (screenWidth * desiredPercentage) / 100;
+    const actualimageHeight = (screenHeight * desiredPercentage) / 100;
+    product.style.padding =  `${imageWidth/ (aspectratio * 40)}px ${imageHeight * aspectratio /40}px`;
+    
+    var currentimageheight = imageHeight * actualimageWidth/imageWidth;
+    var currentimageheight = imageHeight * actualimageHeight/imageHeight;
+  
+    closelabel.style.display = "block";
+    closelabel.style.transform = `translateX(${-0.5 - ((imageWidth/actualimageWidth)*2.4)}vw) translateY(${-((actualimageHeight/2.1))}px)` ;
+  
+    for (const ripple of ripples) {
+      ripple.style.display = "none";
     }
-});
-
-product1.addEventListener("mouseout", function() {
-  for (const circle of circles) {
-    circle.style.display = "block";
-  }
-  for (const product of products) {
-    product.style.display = "block";
-  }
-  loungeimage.style.transform = "scale(1)";
-  loungeimage.style.transformOrigin = "47.3% 59.5%";
+    loungeimage.style.transform = "scale(2)";
+    loungeimage.style.transformOrigin = "47.3% 59.5%";
+    loungeimage.style.filter= "blur(5px)";
+  
+    
+    
+  };
   
 });
-
 
 /*------------------------ Product 2 ------------------------------- */
-product2.addEventListener("mouseover", function() {
-  for (const circle of circles) {
-    circle.style.display = "none";
-  }
-  loungeimage.style.transform = "scale(2)";
-  loungeimage.style.transformOrigin = "54% 69.5%";
-  for (const product of products) {
-    const i=0;
-    i=i+1;
-    if (i==2){continue;}
-    else{ product.style.display = "none"; }
+ripple2.addEventListener("mouseover", function() {
+  product.style.display = "block";
+  product.src = "./img/Product_Images/Von Vene fuente.jpeg";
+  product.onload = function(){
+    const imageHeight = product.naturalHeight;
+    const imageWidth = product.naturalWidth;
+    const aspectratio = imageWidth/imageHeight;
+    product.style.aspectRatio = "" + aspectratio;
+  
+    const screenWidth = window.innerWidth; // Get screen width in pixels
+    const screenHeight = window.innerHeight;
+    const desiredPercentage = 70; // Desired percentage of screen width (Product Image width)
+    const actualimageWidth = (screenWidth * desiredPercentage) / 100;
+    const actualimageHeight = (screenHeight * desiredPercentage) / 100;
+    product.style.padding =  `${imageWidth/ (aspectratio * 40)}px ${imageHeight * aspectratio /40}px`;
+    
+    var currentimageheight = imageHeight * actualimageWidth/imageWidth;
+    var currentimageheight = imageHeight * actualimageHeight/imageHeight;
+  
+    closelabel.style.display = "block";
+    closelabel.style.transform = `translateX(${-0.5 - ((imageWidth/actualimageWidth)*2.4)}vw) translateY(${-((actualimageHeight/2.1))}px)` ;
+  
+    for (const ripple of ripples) {
+      ripple.style.display = "none";
     }
-});
-
-product2.addEventListener("mouseout", function() {
-  for (const circle of circles) {
-    circle.style.display = "block";
-  }
-  for (const product of products) {
-    product.style.display = "block";
-  }
-  loungeimage.style.transform = "scale(1)";
-  loungeimage.style.transformOrigin = "54% 69.5%";
+    loungeimage.style.transform = "scale(2)";
+    loungeimage.style.transformOrigin = "47.3% 59.5%";
+    loungeimage.style.filter= "blur(5px)";
+  
+    
+    
+  };
   
 });
 
-/*------------------------ Product 3 ------------------------------- */
-product3.addEventListener("mouseover", function() {
-  for (const circle of circles) {
-    circle.style.display = "none";
-  }
-  loungeimage.style.transform = "scale(2)";
-  loungeimage.style.transformOrigin = "58% 46%";
-  for (const product of products) {
-    const i=0;
-    i=i+1;
-    if (i==3){continue;}
-    else{ product.style.display = "none"; }
-    }
-});
 
-product3.addEventListener("mouseout", function() {
-  for (const circle of circles) {
-    circle.style.display = "block";
-  }
-  for (const product of products) {
-    product.style.display = "block";
-  }
-  loungeimage.style.transform = "scale(1)";
-  loungeimage.style.transformOrigin = "58% 46%";
-  
-});
-
-/*------------------------ Product 4 ------------------------------- */
-product4.addEventListener("mouseover", function() {
-  for (const circle of circles) {
-    circle.style.display = "none";
-  }
-  loungeimage.style.transform = "scale(2)";
-  loungeimage.style.transformOrigin = "66.1% 46%";
-  for (const product of products) {
-    const i=0;
-    i=i+1;
-    if (i==4){continue;}
-    else{ product.style.display = "none"; }
-    }
-});
-
-product4.addEventListener("mouseout", function() {
-  for (const circle of circles) {
-    circle.style.display = "block";
-  }
-  for (const product of products) {
-    product.style.display = "block";
-  }
-  loungeimage.style.transform = "scale(1)";
-  loungeimage.style.transformOrigin = "66.1% 46%";
-  
-});
-
-/*------------------------ Product 5 ------------------------------- */
-product5.addEventListener("mouseover", function() {
-  for (const circle of circles) {
-    circle.style.display = "none";
-  }
-  loungeimage.style.transform = "scale(2)";
-  loungeimage.style.transformOrigin = "25.3% 69.5%";
-  for (const product of products) {
-    const i=0;
-    i=i+1;
-    if (i==5){continue;}
-    else{ product.style.display = "none"; }
-    }
-});
-
-product5.addEventListener("mouseout", function() {
-  for (const circle of circles) {
-    circle.style.display = "block";
-  }
-  for (const product of products) {
-    product.style.display = "block";
-  }
-  loungeimage.style.transform = "scale(1)";
-  loungeimage.style.transformOrigin = "25.3% 69.5%";
-  
-});
-
-/*------------------------ Product 6 ------------------------------- */
-product6.addEventListener("mouseover", function() {
-  for (const circle of circles) {
-    circle.style.display = "none";
-  }
-  loungeimage.style.transform = "scale(2)";
-  loungeimage.style.transformOrigin = "67% 70.5%";
-  for (const product of products) {
-    const i=0;
-    i=i+1;
-    if (i==6){continue;}
-    else{ product.style.display = "none"; }
-    }
-});
-
-product6.addEventListener("mouseout", function() {
-  for (const circle of circles) {
-    circle.style.display = "block";
-  }
-  for (const product of products) {
-    product.style.display = "block";
-  }
-  loungeimage.style.transform = "scale(1)";
-  loungeimage.style.transformOrigin = "67% 70.5%";
-  
-});
-
-/*------------------------ Product 7 ------------------------------- */
-product7.addEventListener("mouseover", function() {
-  for (const circle of circles) {
-    circle.style.display = "none";
-  }
-  loungeimage.style.transform = "scale(2)";
-  loungeimage.style.transformOrigin = "79% 75.5%";
-  for (const product of products) {
-    const i=0;
-    i=i+1;
-    if (i==7){continue;}
-    else{ product.style.display = "none"; }
-    }
-});
-
-product7.addEventListener("mouseout", function() {
-  for (const circle of circles) {
-    circle.style.display = "block";
-  }
-  for (const product of products) {
-    product.style.display = "block";
-  }
-  loungeimage.style.transform = "scale(1)";
-  loungeimage.style.transformOrigin = "79% 75.5%";
-  
-});
-
-/*------------------------ Product 8 ------------------------------- */
-product8.addEventListener("mouseover", function() {
-  for (const circle of circles) {
-    circle.style.display = "none";
-  }
-  loungeimage.style.transform = "scale(2)";
-  loungeimage.style.transformOrigin = "2% 34%";
-  for (const product of products) {
-    const i=0;
-    i=i+1;
-    if (i==8){continue;}
-    else{ product.style.display = "none"; }
-    }
-});
-
-product8.addEventListener("mouseout", function() {
-  for (const circle of circles) {
-    circle.style.display = "block";
-  }
-  for (const product of products) {
-    product.style.display = "block";
-  }
-  loungeimage.style.transform = "scale(1)";
-  loungeimage.style.transformOrigin = "2% 34%";
-  
-});
 
 
 //--------------------------- Search Bar --------------------------------------
